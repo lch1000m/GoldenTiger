8bef60b0aa074ce1c74eff486ebd246f0a881901
DESKTOP-54T5RVR\Lee ChangHyung <lch1000m@gmail.com>
2018-08-11 10:31:56 +0900

    0.00

---
+++ __init__.py
@@ -0,0 +1,12 @@
+
+from goldentiger.util import *
+
+
+exp1 = [
+    "layer == [9.2,9.5]",
+]
+
+exp4 = [
+    "step_a == ['ppid_b']",
+    "step_b == ['eqp_a']"
+]
---
+++ util.py
@@ -0,0 +1,41 @@
+
+import goldentiger as gt
+
+import pandas as pd
+
+
+def union_list_all(a):
+    assert isinstance(a, list), 'arg a should be list type but {0}'.format(type(a))
+
+    res = []
+
+    for it in a:
+        if res == []:
+            res = it
+        else:
+            res = union_from_list(res, it)
+
+    return res
+
+
+def union_from_list(a, b):
+    assert isinstance(a, list), 'arg a should be list type but {0}'.format(type(a))
+    assert isinstance(b, list), 'arg b should be list type but {0}'.format(type(b))
+
+    return list(set(a) & set(b))
+
+
+def get_wf_from_given_conditiob(df, condition=[]):
+    assert isinstance(df, pd.DataFrame), 'arg df should be df type but {0}'.format(type(df))
+    assert isinstance(condition, list), 'kwarg condition should be list type but {0}'.format(type(condition))
+
+    res = []
+
+    for exp in condition:
+        temp = df.query(exp)
+
+        res.append(temp['lot_wf'].values.tolist())
+
+    result = union_list_all(res)
+
+    return result
